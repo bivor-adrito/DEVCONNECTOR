f4a4631df87a8ea7444b105e93809548193e463a
bivor-adrito <bivor500@gmail.com>
2020-10-08 19:25:42 +0600

    Complete Post Item Component.

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,23 +3,37 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
-const Posts = ({getPosts, post: {posts, loading}}) => {
-    useEffect(() => {
-        getPosts();
-    }, [getPosts])
-  return (<div>
-
-  </div>);
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 class='large text-primary'>Posts</h1>
+      <p class='lead'>
+        <i className='fas fa-user'></i> Welcome to the community!
+      </p>
+      {/*Post Form */}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
-    getPosts: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    post: state.post
-})
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
 
-export default connect(mapStateToProps, {getPosts})(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
